d5fec453ef02e356e99f24b07d69136b0b0339ff
Edison Matos <edison.matos@live.com>
2025-03-06 14:12:27 -0300

    feat: Converte webp e items SEO

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,10 +1,10 @@
-import logo from "../assets/imgs/logo/logo.png";
-import heroImg from "../assets/imgs/hero/lawHero.jpg";
-import imgFeatures from "../assets/imgs/features/imgServices.jpg";
-import aboutImg1 from "../assets/imgs/about/aboutImg.jpg";
-import aboutSocial from "../assets/imgs/about/aboutSocial.jpg";
-import aboutSocialImg from "../assets/imgs/about/aboutSocial.png";
-import imgSteps from "../assets/imgs/steps/imgSteps.jpeg";
+import logo from "../assets/imgs/logo/logo.webp";
+import heroImg from "../assets/imgs/hero/lawHero.webp";
+import imgFeatures from "../assets/imgs/features/imgServices.webp";
+import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
+import aboutSocial from "../assets/imgs/about/aboutSocial.webp";
+import aboutSocialImg from "../assets/imgs/about/aboutSocial.webp";
+import imgSteps from "../assets/imgs/steps/imgSteps.webp";
 
 import bgHeroImg from "../assets/imgs/hero/bgHero.webp";
 import imgAppStore from "../assets/imgs/hero/appStore.webp";
@@ -98,7 +98,10 @@ const content = {
       title: (
         <h1 className="mb-[16px]">
           Trabalhou sem carteira assinada?
-          <span className="text-primary">  Não perca seus direitos trabalhistas!</span>
+          <span className="text-primary">
+            {" "}
+            Não perca seus direitos trabalhistas!
+          </span>
         </h1>
       ),
       subtitle:
@@ -174,8 +177,7 @@ const content = {
       card1: {
         img: featuresImg1,
         title: "Reconhecimento de vínculo empregatício",
-        subtitle:
-          "Garantindo seus direitos quando se trabalha sem registro.",
+        subtitle: "Garantindo seus direitos quando se trabalha sem registro.",
         description: (
           <div>
             A_Definir
